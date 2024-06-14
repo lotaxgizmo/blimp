be4beb34ede073953554f7a70e6c281d3a80ef17
lotaxgizmo <lotaoko2@gmail.com>
2024-06-14 22:23:50 +0100

    dd

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import buybtnabout from '../assets/buybtnabout.svg'
+import aboutus from '../assets/aboutus.svg'
+import aboutimage from '../assets/aboutimage.svg'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='flex flex-col lg:flex-row justify-center lg:justify-around items-center relative mx-4 py-10 '>
+      <div className="left">
+        <img src={aboutus} alt="" className='mb-5' />
+        <img src={buybtnabout} alt="" className='mb-5' />
+      </div>
+
+      <div className="right">
+        <img src={aboutimage} alt="" />
+      </div>
+    </div>
   )
 }
 
